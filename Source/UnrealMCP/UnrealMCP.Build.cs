68a6ba1ac73f29a5fa2b09693d4b10f2094e718b
QAWe-ikun <13610252512@139.com>
2026-03-04 18:41:15 +0800

    add submodule Generative AI Support

--- Source/UnrealMCP/UnrealMCP.Build.cs
+++ Source/UnrealMCP/UnrealMCP.Build.cs
@@ -10,7 +10,7 @@ public class UnrealMCP : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GenerativeAISupport" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
